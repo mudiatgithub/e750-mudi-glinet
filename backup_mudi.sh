be3364013903e7e5801b40734e036a92ba51c7e7
mudiatgithub <145899950+mudiatgithub@users.noreply.github.com>
2023-09-24 20:12:04 +1000

    Create backup_mudi.sh

---
+++ backup_mudi.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+newFolder="/mnt/sda1/backup_$(date +%y%m%d%H%M)"
+
+mkdir "$newFolder"
+
+cd /overlay/upper
+
+tar -cf "$newFolder/overlay_backup.gz" *
+
+cd /
+
+tar --exclude='sys/*' --exclude='run/*' --exclude='tmp/*' --exclude='mnt/*' --exclude='dev/*' --exclude='proc/*' -cf "$newFolder/root_backup.gz" *
