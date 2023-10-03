0bef45984f02030cdc774affd6e84c8edf8d32d6
mudiatgithub <145899950+mudiatgithub@users.noreply.github.com>
2023-10-03 23:20:20 +1000

    Update mudi_restore_backup.sh

--- mudi_restore_backup.sh
+++ mudi_restore_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /mnt/sda1/
-wget https://raw.githubusercontent.com/mudiatgithub/e750-mudi-glinet/main/overlay_backup.gz
+wget https://raw.githubusercontent.com/mudiatgithub/e750-mudi-glinet/main/overlay_backup.gz --no-check-certificate
 cd /overlay/upper
 tar x -f /mnt/sda1/overlay_backup.gz
 reboot
